2097b55b6310e6e6319bc1b8ff03edd076965e32
NguyenTien85 <nmt08051985@gmail.com>
2021-09-01 12:22:12 +0700

    Add User List

--- eShopSolution.AdminApp/Services/IUserApiClient.cs
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -13,5 +13,7 @@ namespace eShopSolution.AdminApp.Services
         Task<string> Authenticate(LoginRequest request);
 
         Task<PagedResult<UserViewModel>> GetUserPaging(GetUserPagingRequest request);
+
+        Task<bool> RegisterUser(RegisterRequest request);
     }
 }
--- eShopSolution.ViewModels/System/Users/Request/RegisterRequest.cs
+++ eShopSolution.ViewModels/System/Users/Request/RegisterRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,19 @@ namespace eShopSolution.ViewModels.System.Users.Request
 {
     public class RegisterRequest
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Birth Day")]
+        [DataType(DataType.Date)]
         public DateTime DoB { get; set; }
+
         public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
         public string UserName { get; set; }
